c4d1169eceebcd122b622152aca738222150d06f
Max <122038527+Vinchziiiii@users.noreply.github.com>
2023-01-20 22:42:38 +0200

    added requests

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import requests
+
+def get_info():
+    response = requests.get(url="https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+
+    with open("Info.txt", "w", encoding="utf-8") as f:
+        f.write(response.text)
+
+    return response.text
+
+
 def main():
-    print()
+    print(get_info())
 
 
 if __name__ == "__main__":
