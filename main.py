ad40fef3ad7dd40b5f18cf292ec9c864049c471f
Vinchziiiiiiiii <122038527+Vinchziiiii@users.noreply.github.com>
2023-02-27 18:35:18 +0200

    Final yopta

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import requests
 
+
 def get_info():
-    response = requests.get(url="https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+    try:
+        response = requests.get(url="https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+    except Exception as err:
+        print(f'Что-то пошло не так: {err}')
+    else:
+        data = response.json()
 
-    with open("Info.txt", "w", encoding="utf-8") as f:
-        f.write(response.text)
+    return data
 
-    return response.text
+
+def pars_js(data_list):
+    eur = data_list[0]
+    usd = data_list[1]
+
+    print(f'''
+            {usd.get("ccy")}
+             Купівля - {usd.get("buy").split('.')[0]} грн. {(usd.get("buy").split('.')[1])[:2]} коп.
+             Продаж - {usd.get("sale").split('.')[0]} грн. {(usd.get("sale").split('.')[1])[:2]} коп.
+            {eur.get("ccy")}
+             Купівля - {eur.get("buy").split('.')[0]} грн. {(eur.get("buy").split('.')[1])[:2]} коп.
+             Продаж - {eur.get("sale").split('.')[0]} грн. {(usd.get("sale").split('.')[1])[:2]} коп.
+
+    ''')
+
+
 
 
 def main():
-    print(get_info())
+    inc_data = get_info()
+    pars_js(inc_data)
 
 
 if __name__ == "__main__":
